9edcfb415e7745ea7f698ea3b88c4cf6f100e91c
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-26 18:34:30 +0600

    handler

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -4,6 +4,7 @@ import { PrismaClient } from "@prisma/client";
 import { prisma } from "../../prisma/db";
 import { typeDefs } from "@/graphql/schema";
 import { resolvers } from "@/graphql/resolvers";
+import allowCors from "@/utils/cors";
 
 export type Context = {
   prisma: PrismaClient;
@@ -11,6 +12,8 @@ export type Context = {
 
 const apolloServer = new ApolloServer<Context>({ typeDefs, resolvers });
 
-export default startServerAndCreateNextHandler(apolloServer, {
+const handler = startServerAndCreateNextHandler(apolloServer, {
   context: async (req, res) => ({ req, res, prisma }),
 });
+
+export default allowCors(handler);
