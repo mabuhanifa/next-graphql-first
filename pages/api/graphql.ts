9ef8f5ec0b51093d0a08b80bbd26cf7b75cf88a8
mabuhanifa <moh.abuhanifa@gmail.com>
2023-09-26 18:29:41 +0600

    resolvers

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -3,15 +3,10 @@ import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { PrismaClient } from "@prisma/client";
 import { prisma } from "../../prisma/db";
 import { typeDefs } from "@/graphql/schema";
+import { resolvers } from "@/graphql/resolvers";
 
 export type Context = {
   prisma: PrismaClient;
-};
-
-const resolvers = {
-  Query: {
-    hello: () => "world",
-  },
 };
 
 const apolloServer = new ApolloServer<Context>({ typeDefs, resolvers });
