5f9602c507369b89f5a6298d7c777f03b4f9a4e9
mabuhanifa <moh.abuhanifa@gmail.com>
2023-10-01 17:12:36 +0600

    app updated

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,11 +1,10 @@
-// import { ApolloServer } from "@apollo/server";
+import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { PrismaClient } from "@prisma/client";
 import { prisma } from "../../prisma/db";
 import { typeDefs } from "@/graphql/schema";
 import { resolvers } from "@/graphql/resolvers";
 import allowCors from "@/utils/cors";
-import { ApolloServer } from "apollo-server-micro";
 
 export type Context = {
   prisma: PrismaClient;
@@ -16,25 +15,8 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// const apolloServer = new ApolloServer<Context>({ typeDefs, resolvers });
-
-// const handler = startServerAndCreateNextHandler(apolloServer, {
-//   context: async (req, res) => ({ req, res, prisma }),
-// });
-
-// export default allowCors(handler);
-
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
-const startServer = server.start();
-
-async function handler(req: any, res: any) {
-  await startServer;
-  await server.createHandler({ path: "/api/graphql" })(req, res);
-}
+const handler = startServerAndCreateNextHandler(server, {
+  context: async (req, res) => ({ req, res, prisma }),
+});
 
 export default allowCors(handler);
